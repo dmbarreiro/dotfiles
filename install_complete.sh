baca1499e91f27e7170ae51a33682006a55619d0
dmbarreiro <5133990+dmbarreiro@users.noreply.github.com>
2017-11-16 12:55:47 +0100

    rm command to delete tpm folder in case it exists before cloning tpm repository

--- install_complete.sh
+++ install_complete.sh
@@ -7,6 +7,7 @@ cp tmux.conf ~/.tmux.conf
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+rm -rf ~/.tmux/plugins/tpm
 git clone git://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Configurations and plugin managers successfully installed."
